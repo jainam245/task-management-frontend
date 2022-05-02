92e9d020b177b40e78702c1a0dda8274c513a821
Jainam <jainamdoshi245@gmail.com>
2022-05-02 16:12:11 +0530

    deploy

---
+++ build/precache-manifest.2a226ea0c6ad4bc4ed9fcd04fe4501f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ecbf1ab304f6b925b7b4554a167f6f",
+    "url": "/task-management-frontend/index.html"
+  },
+  {
+    "revision": "1ceff45f885a3d68ae02",
+    "url": "/task-management-frontend/static/css/main.eee21bd5.chunk.css"
+  },
+  {
+    "revision": "f955cea14b2a5137a5d5",
+    "url": "/task-management-frontend/static/js/2.ecd04186.chunk.js"
+  },
+  {
+    "revision": "1ceff45f885a3d68ae02",
+    "url": "/task-management-frontend/static/js/main.56fb99ea.chunk.js"
+  },
+  {
+    "revision": "170cb679b8a6db4918f5",
+    "url": "/task-management-frontend/static/js/runtime~main.961a32b0.js"
+  }
+]);
