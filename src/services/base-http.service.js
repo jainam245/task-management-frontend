fd85fc0e5561e5e2ccaf242219893743ed3a0e56
Jainam <jainamdoshi245@gmail.com>
2022-05-07 14:44:52 +0530

    or

--- src/services/base-http.service.js
+++ src/services/base-http.service.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export default class BaseHttpService {
   BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3000';
+  
   _accessToken = null;
 
   constructor(routerStore) {
